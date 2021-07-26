30220fb2976b5685709be2ec00d5413f06fc49e1
nickFalcone <nfalcone@u.rochester.edu>
2021-07-25 23:13:27 -0400

    add About component

---
+++ src/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <footer>
+      Based on the{" "}
+      <a
+        className="footer-link"
+        href="https://www.casio.com/products/watches/classic/f91w-1"
+      >
+        CASIO F91W-1
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { render } from "react-dom";
 import Clock from "./Clock";
+import About from "./About";
 
 const App = () => {
   return (
     <div>
       <Clock brand="CASIO" model="F-91W" />
+      <About />
     </div>
   );
 };
