ad283b41a1771c79357091fb1380def020159b47
nickFalcone <nfalcone@u.rochester.edu>
2021-07-26 00:35:47 -0400

    Use hourCycle h23 so that midnight is represented as 00 instead of 24

--- src/HourMinute.js
+++ src/HourMinute.js
@@ -1,14 +1,16 @@
 const HourMinute = (props) => {
   return (
     <time className="time">
-      {/* Remove AM/PM from the time string - it is displayed above */}
       {props.twentyFourHour
-        ? props.time
-            .toLocaleTimeString("en-US", { hour12: !props.twentyFourHour })
-            .slice(0, -3)
+        ? props.time.toLocaleTimeString("en-US", {
+            hourCycle: "h23",
+            hour: "2-digit",
+            minute: "2-digit",
+          })
         : props.time
             .toLocaleTimeString("en-US", { hour12: !props.twentyFourHour })
             .slice(0, -6)}
+      {/* Remove AM/PM from the time string - it is displayed above */}
       <span className="seconds">
         {props.time.getSeconds().toLocaleString("en-US", {
           minimumIntegerDigits: 2,
