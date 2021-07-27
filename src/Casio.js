ec9d7fceddbffaa39dad14f19a965a4eec7ec1da
nickFalcone <nfalcone@u.rochester.edu>
2021-07-27 15:28:27 -0400

    Casio easter egg when alarm button held for 3 seconds

---
+++ src/Casio.js
@@ -0,0 +1,9 @@
+const Casio = () => {
+  return (
+    <div className="casio hide">
+      <p>CASIo</p>
+    </div>
+  );
+};
+
+export default Casio;
--- src/Clock.js
+++ src/Clock.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import DayDate from "./DayDate";
 import LightButton from "./LightButton";
 import HourMinute from "./HourMinute";
+import Casio from "./Casio";
 
 const Clock = (props) => {
   const [time, setTime] = useState(new Date());
@@ -11,6 +12,28 @@ const Clock = (props) => {
     const updateTime = setTimeout(() => setTime(new Date()), 1000);
     return () => clearTimeout(updateTime);
   });
+
+  const mouseDownHandler = () => {
+    const dayDate = document.querySelector(".day-date");
+    const time = document.querySelector(".time");
+    const casio = document.querySelector(".casio");
+    // if button is held for 3 seconds, display CASIO message for 3 seconds.
+    holdTimer = setTimeout(() => {
+      dayDate.classList.add("hide");
+      time.classList.add("hide");
+      casio.classList.remove("hide");
+      setTimeout(() => {
+        dayDate.classList.remove("hide");
+        time.classList.remove("hide");
+        casio.classList.add("hide");
+      }, 3000);
+    }, 3000);
+  };
+
+  const mouseUpHandler = () => {
+    clearTimeout(holdTimer);
+  };
+
   return (
     <div id="outerFace">
       <div className="make-model">
@@ -20,13 +43,18 @@ const Clock = (props) => {
       <span className="feature">ALARM CHRONOGRAPH</span>
       <div id="lcdFace">
         <div className="black-border">
+          <Casio />
           <DayDate time={time} twentyFourHour={twentyFourHour} />
           <HourMinute time={time} twentyFourHour={twentyFourHour} />
         </div>
       </div>
       <p>
         MODE
-        <button onClick={() => setTwentyFourHours(!twentyFourHour)}>
+        <button
+          onClick={() => setTwentyFourHours(!twentyFourHour)}
+          onMouseDown={mouseDownHandler}
+          onMouseUp={mouseUpHandler}
+        >
           ALARM ON&sdot;OFF / 24HR
         </button>
       </p>
